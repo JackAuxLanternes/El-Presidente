c43f6485f7966d1ad56590dc0ca06ec55d96c215
Quentin MOLERO <quentin.molero@estiam.com>
2021-02-05 16:22:28 +0100

    Complete scenario loading for existing objects, minor modifications in Game.

--- src/main/java/fr/elpresidente/game/scenario/ScenarioLoader.java
+++ src/main/java/fr/elpresidente/game/scenario/ScenarioLoader.java
@@ -1,4 +1,58 @@
 package fr.elpresidente.game.scenario;
 
+import fr.elpresidente.game.factions.Faction;
+import fr.elpresidente.game.factions.FactionController;
+import fr.elpresidente.game.resources.ConsumableController;
+import fr.elpresidente.game.resources.ResourcesController;
+import fr.elpresidente.game.tools.JSONTools;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class ScenarioLoader {
+
+    private Scenario scenario;
+
+    public ScenarioLoader(Scenario scenario) {
+        this.setScenario(scenario);
+    }
+
+    public void tryToLoadScenario() {
+        try {
+            this.loadResourceScenario();
+            this.loadConsumableScenario();
+            this.loadFactionsScenario();
+        } catch (NullPointerException nullPointerException) {
+            throw new NullPointerException("Couldn't load scenario completely, check file is in the right folder and not corrupted.\n" + nullPointerException);
+        }
+    }
+
+    public void loadResourceScenario() {
+        JSONObject agricultureScenario = JSONTools.findJSONObjectInJSONArrayWithKeyValue(scenario.getResources(), "name", "agriculture");
+        JSONObject industryScenario = JSONTools.findJSONObjectInJSONArrayWithKeyValue(scenario.getResources(), "name", "industry");
+
+        ResourcesController.getInstance().getAgriculture().setSize(JSONTools.extractIntFromJSONObject(agricultureScenario, "value"));
+        ResourcesController.getInstance().getIndustry().setSize(JSONTools.extractIntFromJSONObject(industryScenario, "value"));
+    }
+
+    public void loadConsumableScenario() {
+        JSONObject foodScenario = JSONTools.findJSONObjectInJSONArrayWithKeyValue(scenario.getConsumable(), "name", "food");
+        JSONObject treasuryScenario = JSONTools.findJSONObjectInJSONArrayWithKeyValue(scenario.getConsumable(), "name", "treasury");
+
+        ConsumableController.getInstance().getFood().setAmount(JSONTools.extractIntFromJSONObject(foodScenario, "value"));
+        ConsumableController.getInstance().getTreasury().setAmount(JSONTools.extractIntFromJSONObject(treasuryScenario, "value"));
+    }
+
+    public void loadFactionsScenario() {
+        JSONArray factionsScenario = scenario.getFactions();
+
+        for (Faction faction : FactionController.getInstance().getFactions()) {
+            JSONObject scenarioFaction = JSONTools.findJSONObjectInJSONArrayWithKeyValue(factionsScenario, "name", faction.getName());
+            faction.setSatisfaction(JSONTools.extractIntFromJSONObject(scenarioFaction, "popularity"));
+            faction.setSupporters(JSONTools.extractIntFromJSONObject(scenarioFaction, "supporters"));
+        }
+    }
+
+    private void setScenario(Scenario scenario) {
+        this.scenario = scenario;
+    }
 }
--- src/main/java/fr/elpresidente/game/status/Game.java
+++ src/main/java/fr/elpresidente/game/status/Game.java
@@ -1,15 +1,16 @@
 package fr.elpresidente.game.status;
 
+import fr.elpresidente.game.factions.Faction;
 import fr.elpresidente.game.factions.FactionController;
 import fr.elpresidente.game.resources.ConsumableController;
 import fr.elpresidente.game.resources.ResourcesController;
 import fr.elpresidente.game.scenario.Scenario;
+import fr.elpresidente.game.scenario.ScenarioLoader;
 import fr.elpresidente.game.tools.JSONTools;
 import fr.elpresidente.game.turn.Seasons;
 import fr.elpresidente.game.turn.TurnController;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 
 public class Game {
 
@@ -33,16 +34,8 @@ public class Game {
     }
 
     public void initFromScenario(Scenario scenario) {
-        System.out.println(
-                scenario.getResources().stream().filter(resource -> ((JSONObject) JSONValue.parse(resource.toString())).get("name").equals("agriculture")).findFirst()
-                .stream().mapToInt(resource -> JSONTools.extractIntFromJSONObject((JSONObject) resource, "value")).sum()
-        );
-        for (int i = 0; i < scenario.getResources().size(); i++) {
-            JSONObject resource = (JSONObject) scenario.getResources().get(i);
-            if (resource.containsKey("name") && resource.containsValue("agriculture")) {
-                ResourcesController.getInstance().getAgriculture().setSize(Math.toIntExact((long) resource.get("value")));
-            }
-        }
+        ScenarioLoader scenarioLoader = new ScenarioLoader(scenario);
+        scenarioLoader.tryToLoadScenario();
     }
 
     public void gameLoop() {
