98844b5f26fda24ba817af25b9db1ac4ee221876
Quentin <rouvilleq@gmail.com>
2021-03-07 13:08:27 +0100

    Add sandbox.json with different starting values

--- src/main/java/fr/elpresidente/game/Main.java
+++ src/main/java/fr/elpresidente/game/Main.java
@@ -5,6 +5,8 @@ import fr.elpresidente.game.JSON.scenario.Scenario;
 import fr.elpresidente.game.launcher.GameLauncher;
 import fr.elpresidente.game.launcher.LauncherGameType;
 import fr.elpresidente.game.launcher.LauncherMenu;
+import fr.elpresidente.game.mode.GameModeController;
+import fr.elpresidente.game.mode.SandboxMode;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,8 +16,13 @@ public class Main {
         try {
             JSONParser scenarioParser = new JSONParser("./scenario.json");
             LauncherGameType gameType = launcherMenu.choseGameType();
-            if (gameType == LauncherGameType.NEW_GAME) {
+            if (gameType == LauncherGameType.NEW_GAME)
+            {
                 launcherMenu.choseGameMode();
+                if(GameModeController.getInstance().getGameMode() instanceof SandboxMode)
+                {
+                    scenarioParser = new JSONParser("./sandbox.json");
+                }
                 launcherMenu.choseDifficultyForTheGame();
                 scenarioParser.openAsScenario();
                 gameLauncher.createNewGame((Scenario) scenarioParser.getContent());
